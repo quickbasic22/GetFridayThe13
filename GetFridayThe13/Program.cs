ae964249e39807081a8bc01e2fdda10983b9b803
David Morrow <quickbasic44@outlook.com>
2021-09-19 19:54:01 -0400

    Add project files.

---
+++ GetFridayThe13/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetFridayThe13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            DateTime cur;
+            DateTime[] dayArray = new System.DateTime[17];
+            int totalDays = 3650;
+            int dayArrayCount = 0;
+            for (int day = 1; day < totalDays; day++)
+            {
+                cur = today.AddDays(day);
+                if (IsFriday13(cur))
+                {
+                    dayArray[dayArrayCount] = cur;
+                    dayArrayCount++;
+                }
+            }
+
+            foreach (DateTime dt in dayArray)
+            {
+                Console.WriteLine(dt.ToShortDateString());
+            }
+            Console.WriteLine($"dayArraycount = {dayArrayCount}");
+            Console.ReadKey();
+        }
+
+        private static bool IsFriday13(DateTime day)
+        {
+            if (day.DayOfWeek == System.DayOfWeek.Friday && day.Day == 13)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
